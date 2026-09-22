6721cec8bce093f6863a7921bcefbb7323b0f1e0
Wildan Fariezky Maaruf <dracko.maaruftm@gmail.com>
2026-09-22 22:05:12 +0700

    Add: is-anagram - submission-60

---
+++ Data Structures & Algorithms/is-anagram/submission-60.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counterT = {}
+        counterS = {}
+
+        for c in s:
+            counterT[c] = counterT.get(c , 0) + 1
+        for c in t:
+            counterS[c] = counterS.get(c  , 0) + 1
+
+            
+        return counterS == counterT
+        
